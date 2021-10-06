4e59a4beb72debddfb7045f327a1ad9fec1c6671
Irfan <fahimirfan70@gmail.com>
2021-10-06 12:42:38 +0600

    changed settings for local server

--- data_got/settings.py
+++ data_got/settings.py
@@ -88,10 +88,10 @@ WSGI_APPLICATION = 'data_got.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dieseiner_data', 
-        'USER': 'postgres', 
+        'NAME': 'data_got',
+        'USER': 'postgres',
         'PASSWORD': 'SuperSecretPassword',
-        'HOST': 'localhost', 
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
@@ -134,10 +134,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
-LOGIN_REDIRECT_URL = 'https://dieseinerdata.com/visualisation/data-visualisation/'
+LOGIN_REDIRECT_URL = 'http://localhost:8888/visualisation/data-visualisation/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
